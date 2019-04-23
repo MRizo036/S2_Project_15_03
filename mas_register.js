602e2aaec20cf742f1ec57b98f7d637383395dc9
Unknown <mrizo036@west-mec.org>
2019-04-22 18:26:11 -0700

    Functions Completed
    All JavaScript functions completed in mas_register.js. Error concerning sessionStorage must be resolved.

--- mas_register.js
+++ mas_register.js
@@ -45,14 +45,54 @@ window.addEventListener("load", function () {
 });
 
 function sessionTest() {
-      var confSession = document.getElementById("sessionBox");
+      sessionStorage.confSession = document.getElementById("sessionBox");
       if (confSession.selectedIndex === -1) {
-            confSession
-
+            confSession.setCustomValidity("Select a Session Package");
+      } else {
+            confSession.setCustomValidity("");
       }
 
 }
 
 function calcCart() {
+      sessionStorage.confName = document.getElementById("fnBox").value + " " + document.getElementById("lnBox").value;
+
+      sessionStorage.confGroup = document.getElementById("groupBox").value;
+      sessionStorage.confMail = document.getElementById("mailBox").value;
+      sessionStorage.confPhone = document.getElementById("phoneBox").value;
+      sessionStorage.confBanquet = document.getElementById("banquetBox").value;
+
+      sessionStorage.confBanquetCost = sessionStorage.confBanquet * 55;
+
+      sessionStorage.selectedIndex = document.forms.regForm.elements.sessionBox.value;
+      if (sessionBox.selectedIndex !== -1) {
+            sessionStorage.confSession = document.forms.regForm.elements.sessionBox[selectedIndex].textContent;
+            sessionStorage.confSessionCost = document.forms.regForm.elements.sessionBox.value;
+      } else {
+            sessionStorage.confSession = "";
+            sessionStorage.confSessionCost = 0;
+      }
+
+      if (document.getElementById("mediaCB").onclick) {
+            sessionStorage.confPack = "yes";
+            sessionStorage.confPackCost = 115;
+      } else {
+            sessionStorage.confPack = "no"
+            sessionStorage.confPackCost = 0;
+      }
+
+      sessionStorage.confTotal = parseFloat(sessionStorage.confBanquetCost) + parseFloat(sessionStorage.confSessionCost) + parseFloat(sessionStorage.confPackCost);
+
+      writeSessionValues();
+}
+
+function writeSessionValues() {
+      sessionStorage.confName = document.getElementById("regName");
+      sessionStorage.confGroup = document.getElementById("regGroup");
+      sessionStorage.confMail = document.getElementById("regEmail");
+      sessionStorage.confPhone = document.getElementById("regPhone");
+      sessionStorage.confSession = document.getElementById("regSession");
+      sessionStorage.confBanquet = document.getElementById("regBanquet");
+      sessionStorage.confPack = document.getElementById("regPack");
 
 }
